d3075b86d5f9d96a8768f8d02766f435303a502e
Jeremy Derr <jeremy@derr.me>
2013-08-09 09:26:25 -0500

    initial commit

---
+++ elections/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+LOOT_TYPES = [
+    {'W': 'Weapon'},
+    {'A': 'Armor'},
+    {'O': 'Other'},
+]
+
+
+class Loot(models.Model):
+    name = models.CharField(max_length=45)
+    loot_type = models.CharField(max_length=5, choices=LOOT_TYPES)
+    bonus = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Election(models.Model):
+    user = models.EmailField()
+    weight = models.IntegerField()
+    loot = models.ForeignKey(Loot)
+    awarded = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        if not self.awarded:
+            return '{} request: {} ({})'.format(
+                self.user, self.loot.name, self.weight)
+        else:
+            return '{} received {}'.format(self.user, self.loot.name)
