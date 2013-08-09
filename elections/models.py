a8401b3c9a2eeddbfbb99bfd1238024e0965318c
Jeremy Derr <jeremy.derr@mutualmobile.com>
2013-08-09 13:57:55 -0500

    branching

--- elections/models.py
+++ elections/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 LOOT_TYPES = [
-    {'W': 'Weapon'},
-    {'A': 'Armor'},
-    {'O': 'Other'},
+    ['W', 'Weapon'],
+    ['A', 'Armor'],
+    ['O', 'Other'],
 ]
 
 
@@ -13,6 +13,7 @@ class Loot(models.Model):
     name = models.CharField(max_length=45)
     loot_type = models.CharField(max_length=5, choices=LOOT_TYPES)
     bonus = models.IntegerField()
+    owner = models.CharField(max_length=40)
 
     def __unicode__(self):
         return self.name
@@ -23,6 +24,7 @@ class Election(models.Model):
     weight = models.IntegerField()
     loot = models.ForeignKey(Loot)
     awarded = models.BooleanField(default=False)
+    comment = models.CharField(max_length=140)
 
     def __unicode__(self):
         if not self.awarded:
